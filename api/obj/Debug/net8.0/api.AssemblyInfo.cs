56ac1b755e781f9cd17cbb472f20ee38e9b0dbe1
BrunooSilva13 <brunomendes.2259@gmail.com>
2024-03-28 14:34:27 -0300

    conexão com banco

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9697747949a032fe431d64ee80e070ddd188614f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01b1b40d87ac36bf86e30e2ab1b9badf81cd31f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
