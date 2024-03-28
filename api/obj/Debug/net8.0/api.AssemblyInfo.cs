43a5bcd7361ee750d9b87a5c798d7f95bc26bba5
BrunooSilva13 <brunomendes.2259@gmail.com>
2024-03-28 18:50:06 -0300

    git ignore

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80d363ab62cfe028f4f63c1695ea863e3f82be7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2094a0206eff7ebad6da7a03af3044c2bc8daa9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
